9875b833d942f777d15313e46a5994ea6f9b0b45
Rana Sherif <ranasherif2007@gmail.com>
2025-12-07 01:22:35 +0200

    Checkout

---
+++ app/models/Checkout.py
@@ -0,0 +1,18 @@
+class Checkout:
+    def __init__(self, shipping_address, Phone_number, payement_method , items):
+        self.shipping_address = shipping_address
+        self.Phone_number = Phone_number
+        self.payement_method = payement_method
+        self.items = items
+    def order_summary(self):
+        subtotal = sum(item.price*item.quantity for item in self.items)
+        shipping_fee = 50 if subtotal > 100 else 0
+        total = subtotal + shipping_fee
+        return{
+            "Shipping Address": self.shipping_address,
+            "Phone Number": self.Phone_number, 
+            "payement Method": self.payement_method,
+            "subtotal": subtotal,
+            "shipping_fee": shipping_fee,
+            "total": total
+        }
