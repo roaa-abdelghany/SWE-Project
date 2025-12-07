64d2a8b9e9096a7c67134873b9faee1260199906
roaaa <roaaabdelghny39@gmail.com>
2025-12-07 06:25:48 +0200

    Cart

--- app/app.py
+++ app/app.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from controllers.product_controller import product_bp
+from controllers.cart_controller import cart_bp
+from controllers.Checkout_controller import checkout_bp
+
+
+app = Flask(__name__)
+app.register_blueprint(checkout_bp) 
+
+app = Flask(__name__)
+app.register_blueprint(cart_bp) 
 
 app = Flask(__name__)
 app.register_blueprint(product_bp) 
