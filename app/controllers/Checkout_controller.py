d70ffb03e8831fdfa1892c4798a65b8fb8d957db
Rana Sherif <ranasherif2007@gmail.com>
2025-12-10 13:58:32 +0200

    checkout

--- app/controllers/Checkout_controller.py
+++ app/controllers/Checkout_controller.py
@@ -7,7 +7,17 @@ checkout_bp = Blueprint('checkout', __name__, url_prefix='/checkout')
 @checkout_bp.route("/<int:user_id>", methods=["GET"])
 def show_checkout_page(user_id):
     items = CartRepository.get_by_user(user_id)
-    return render_template("checkout.html", items=items, user_id=user_id)
+    subtotal = sum(item.price * item.quantity for item in items)
+    shipping_fee = 0 if (subtotal >= 100 or subtotal==0) else 50
+    total = subtotal + shipping_fee
+    return render_template(
+        "checkout.html",
+        items=items,
+        user_id=user_id,
+        subtotal=subtotal,
+        shipping_fee=shipping_fee,
+        total=total
+    )
 
 @checkout_bp.route("/<int:user_id>", methods=["POST"])
 def process_checkout(user_id):
@@ -20,4 +30,4 @@ def process_checkout(user_id):
     checkout = Checkout(shipping_address, phone_number, payment_method, items)
     order_summary = checkout.order_summary()
 
-    return render_template("order_confirmation.html", order_summary=order_summary)
+    return render_template("order_confirmation.html",**order_summary )
