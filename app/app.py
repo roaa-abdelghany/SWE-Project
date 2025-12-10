5a6ddc5ad78accb0eefeb2765b58442c9eb6f284
Rana Sherif <ranasherif2007@gmail.com>
2025-12-10 22:19:18 +0200

    checkout

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
-from controllers.product_controller import product_bp
+from flask import Flask, render_template
+#from controllers.cart_controller import cart_bp
+#from controllers.product_controller import product_bp
+from controllers.Checkout_controller import checkout_bp
 
 app = Flask(__name__)
-app.register_blueprint(product_bp) 
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+#app.register_blueprint(product_bp)      
+#app.register_blueprint(cart_bp)        
+app.register_blueprint(checkout_bp)     
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/controllers/Checkout_controller.py
+++ app/controllers/Checkout_controller.py
@@ -7,17 +7,10 @@ checkout_bp = Blueprint('checkout', __name__, url_prefix='/checkout')
 @checkout_bp.route("/<int:user_id>", methods=["GET"])
 def show_checkout_page(user_id):
     items = CartRepository.get_by_user(user_id)
-    subtotal = sum(item.price * item.quantity for item in items)
-    shipping_fee = 0 if (subtotal >= 100 or subtotal==0) else 50
-    total = subtotal + shipping_fee
+    Checkout= Checkout("", "", "", items)
+    order_summary = Checkout.order_summary()
     return render_template(
-        "checkout.html",
-        items=items,
-        user_id=user_id,
-        subtotal=subtotal,
-        shipping_fee=shipping_fee,
-        total=total
-    )
+        "checkout.html", items=items, user_id=user_id,subtotal=order_summary["subtotal"],shipping_fee=order_summary["shipping_fee"],total=order_summary["total"])
 
 @checkout_bp.route("/<int:user_id>", methods=["POST"])
 def process_checkout(user_id):
